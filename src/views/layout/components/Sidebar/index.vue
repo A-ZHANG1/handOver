df816caa7d55f0aeb2b6840633656ff7242bb8f7
cn_yyf <cn_yyf@163.com>
2018-12-03 21:15:47 +0800

    修改导航栏颜色

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -5,9 +5,9 @@
       :default-active="$route.path"
       :collapse="isCollapse"
       mode="vertical"
-      background-color="#304156"
-      text-color="#bfcbd9"
-      active-text-color="#409EFF"
+      background-color="#fafafa"
+      text-color="#707070"
+      active-text-color="#393982"
     >
       <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path"/>
     </el-menu>
